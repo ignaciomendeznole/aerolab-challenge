89c384886523da66423ad5472ed5c55e82f4ab83
Ignacio Mendez <>
2021-09-03 17:00:58 -0300

    axios typing

--- config/axiosClient.tsx
+++ config/axiosClient.tsx
@@ -1,14 +1,14 @@
-import axios from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
 const baseURL = 'https://coding-challenge-api.aerolab.co/';
 const token =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTMwZTBlMGE5MDUzOTAwMjFiNzBiMjAiLCJpYXQiOjE2MzA1OTMyNDh9.6NAySmApXVQcpTEhMVnqozktGY86yMAmrC9Iwv7C7xM';
 
-const axiosClient = axios.create({
+const axiosClient: AxiosInstance = axios.create({
   baseURL,
 });
 
-axiosClient.interceptors.request.use(async (config) => {
+axiosClient.interceptors.request.use(async (config: AxiosRequestConfig) => {
   config.headers['Authorization'] = `Bearer ${token}`;
   return config;
 });
